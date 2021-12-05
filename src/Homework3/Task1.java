63afa537e156b88a25632a292be22e50df8d7f28
Аркадий <arkady.galperin@gmail.com>
2021-12-05 12:33:21 +0300

    Completed 100%

--- src/Homework3/Task1.java
+++ src/Homework3/Task1.java
@@ -2,15 +2,19 @@ package Homework3;
 
 public class Task1 {
     public static void main(String[] args) {
-        int n1=0,n2=1,n3,i,count=10;
-        System.out.print(n1+" "+n2);//printing 0 and 1
+        int n1=0;
+        int n2=1;
+        int n3;
+        int i;
+        int count=10;
+        System.out.print(n1 + " " + n2); //выводим 0 и 1
 
-        for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed
+        for (i = 2; i < count; i++) //начинаем цикл с двух ибо 0 и 1 уже вывели
             {
-                n3=n1+n2;
-                System.out.print(" "+n3);
-                n1=n2;
-                n2=n3;
+                n3 = n1 + n2;
+                System.out.print(" " + n3);
+                n1 = n2;
+                n2 = n3;
                 }
             }
 }
